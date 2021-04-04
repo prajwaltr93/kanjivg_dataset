8fc859bbbf7b25fcd79b23bab941c728bd99dd27
Prajwal T R <prajwaltr93@gmail.com>
2021-04-04 16:36:39 +0530

    remove un-nessasary m indices lookup, improves performance

--- global_strokegenerator.py
+++ global_strokegenerator.py
@@ -8,6 +8,7 @@
 '''
 import numpy as np
 from drawing_utils import *
+import copy as cp
 
 traverse_path = "./kanji_modified/"
 
@@ -15,7 +16,7 @@ traverse_path = "./kanji_modified/"
 def strokeGenerator(filelist, dataaug = False):
     for file in filelist:
         svg_string = open(traverse_path+file).read()
-        X_target, m_indices = getStrokesIndices(svg_string)
+        X_target = getStrokes(svg_string)
         # last point of local model
         X_loc = Point()
         # strokes completed by local model
@@ -23,18 +24,20 @@ def strokeGenerator(filelist, dataaug = False):
         # last stroke drawn by local model
         X_last = []
         # remaining strokes to complete
-        X_diff = X_target
+        X_diff = cp.deepcopy(X_target)
         #target label
         label = Point()
-        label.updatePoint(X_target[0])
+        label.updatePoint(X_target[0][0])
         # creating images and labels
-        for index, m_index in zip(range(len(m_indices)), m_indices.__iter__()):
+        for ind in range(len(X_target)):
             # each function call generates corresponding image
+            stroke = X_target[ind]
             X_loc_img = drawPoint(X_loc)
             X_env_img = drawStroke(X_env)
             X_last_img = drawStroke(X_last)
             X_diff_img = drawStroke(X_diff)
             X_label_img = drawPoint(label)
+
             yield np.dstack((X_loc_img, X_env_img, X_last_img, X_diff_img)), np.reshape(X_label_img, (HEIGHT * WIDTH))
             # configure image data augementation if specified
             auggen = ImageGen(width_shift=[-2, 3, 1], height_shift=[-2, 3, 1]) if dataaug else None # 5 steps from -2 to 2 total of 5 augmented images applied randomly.
@@ -44,11 +47,10 @@ def strokeGenerator(filelist, dataaug = False):
                 for X_loc_img, X_env_img, X_last_img, X_diff_img, X_label_img in auggen:
                     yield np.dstack((X_loc_img, X_env_img, X_last_img, X_diff_img)), np.reshape(X_label_img, (HEIGHT * WIDTH))
             # udpate variables
-            if (len(m_indices) == 1) or (index + 1 == len(m_indices)):
-                # X_target has only one stroke
-                continue
-            X_loc.updatePoint(X_target[m_indices[index + 1] - 1])
-            X_env += X_target[m_indices[index] : m_indices[index + 1]]
-            X_last = X_target[m_indices[index] : m_indices[index + 1]]
-            X_diff = X_target[m_indices[index + 1]:]
-            label.updatePoint(X_target[m_indices[index + 1]])
+            if(ind+1 == len(X_target)): # reached end of strokes
+                continue # excluding last step of argmax(label) = 0 TODO : consider this step also ?
+            X_loc.updatePoint(stroke[-1])
+            X_env += [stroke]
+            X_last = [stroke]
+            X_diff.pop(0) # remove first stroke
+            label.updatePoint(X_target[ind +1][0]) # next stroke first cordinate
